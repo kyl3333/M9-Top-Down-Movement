5726de19d98629284ebaa1151f084c1e455a9220
kyl3333 <kylee@s.sfusd.edu>
2025-12-10 11:25:58 -0800

    8 finies

--- M09.top_down_movement_workbook/lessons/runner.gd
+++ M09.top_down_movement_workbook/lessons/runner.gd
@@ -1,4 +1,6 @@
 extends CharacterBody2D
+class_name Runner
+signal walked_to
 
 @onready var _dust: GPUParticles2D = %Dust
 @onready var _runner_visual: RunnerVisual = %RunnerVisualRed
@@ -29,4 +31,20 @@ func _physics_process(delta: float) -> void:
 	else:
 		_runner_visual.animation_name = RunnerVisual.Animations.IDLE
 		_dust.emitting = false
+	
+func walk_to(destination_global_position: Vector2) -> void:
+	var direction := global_position.direction_to(destination_global_position)
+	_runner_visual.angle = direction.orthogonal().angle()
+	_runner_visual.animation_name = RunnerVisual.Animations.WALK
+	_dust.emitting = true
+	var distance := global_position.distance_to(destination_global_position)
+	var duration :=  distance / (max_speed * 0.2)
+	var tween := create_tween()
+	tween.tween_property(self, "global_position", destination_global_position, duration)
+	tween.finished.connect(func():
+		_runner_visual.animation_name = RunnerVisual.Animations.IDLE
+		_dust.emitting = false
+		walked_to.emit()
+	)
+	
 	
